a2167b0b8234e00e4ae1a51fe170bf2dd7e928e6
Andres <andres.quintero98@hotmail.com>
2020-03-11 11:14:30 -0500

    confiduracion base de datos y archivos mybatis

---
+++ src/main/java/edu/eci/arsw/findeci/services/FindEciServicesFactory.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.findeci.services;
+import com.google.inject.Injector;
+import com.google.inject.Guice;
+import org.mybatis.guice.XMLMyBatisModule;
+import org.mybatis.guice.datasource.helper.JdbcHelper;
+/**
+ *
+ * @author Andrés Quintero
+ */
+public class FindEciServicesFactory {
+    private static FindEciServicesFactory instance = new FindEciServicesFactory();
+
+    private static Injector injector;
+
+    private static Injector testInjector;
+
+    public FindEciServicesFactory(){
+
+        injector = Guice.createInjector(new XMLMyBatisModule() {
+            @Override
+            protected void initialize() {
+                install(JdbcHelper.PostgreSQL);
+                setClassPathResource("mybatis-config.xml");
+                
+            }
+        });
+
+        testInjector = Guice.createInjector(new XMLMyBatisModule() {
+            @Override
+            protected void initialize() {
+                install(JdbcHelper.PostgreSQL);
+                setClassPathResource("mybatis-config-h2.xml");
+                
+            }
+        });
+
+    }
+
+    public FindEciServices getHistoryService(){
+        return injector.getInstance(FindEciServices.class);
+    }
+
+    public FindEciServices getHistoryServiceForTesting(){
+        return testInjector.getInstance(FindEciServices.class);
+    }
+
+    public static FindEciServicesFactory getInstance(){
+        return instance;
+    }
+
+}
