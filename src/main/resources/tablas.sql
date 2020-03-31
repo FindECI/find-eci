ac188ef98676707c1d831f06e43af07e37e97c45
jimmy chirivi <jimmychirivi09@gmail.com>
2020-03-30 20:27:35 -0500

    modificacion de base de datos

--- src/main/resources/tablas.sql
+++ src/main/resources/tablas.sql
@@ -1,50 +1,75 @@
 create table usuarios(
 nombre varchar(100) not null,
-correo varchar(100) primary key,
-contraseña varchar(100) not null,
-edad numeric(2) not null,
+apellido varchar(100) not null,
+correo varchar(100) not null,
+contrasena varchar(100) not null,
+fecha_nacimiento date not null,
 sexo varchar(20) not null,
 altura numeric(3) not null,
 celular numeric(10) not null,
 cuentaIg varchar(100) not null);
 
+alter table usuarios add constraint pk_usuarios primary key (correo);
+
 create table carrera(
 nombre varchar(100) not null,
 semestre numeric(2) not null,
-añoIngreso numeric(4) not null,
-usuario varchar(100) references usuarios(correo));
+Ingreso numeric(4) not null,
+usuario varchar(100)not null);
+
+alter table carrera add constraint fk_carrera foreign key (usuario) references usuarios(correo);
 
 create table fetiches(
-idFetiche serial primary key,
+idFetiche numeric(100) not null,
 nombre varchar(100) not null,
 descripcion varchar(500),
-usuario varchar(100) references usuarios(correo));
+usuario varchar(100) not null);
 
-create table gustosComida(
-idGustoComida serial primary key,
-nombre varchar(100) not null,
-descripcion varchar(500),
-usuario varchar(100) references usuarios(correo));
+alter table fetiches add constraint pk_fetiches primary key (idfetiche);
+alter table fetiches add constraint fk_fetichesf foreign key (usuario) references usuarios(correo);
 
-create table gustosPeliculas(
-idGustoPelicula serial primary key,
-nombre varchar(100) not null,
-descripcion varchar(500),
-usuario varchar(100) references usuarios(correo));
+create table tipo_gusto(
+id_tipoGusto numeric(10) not null,
+tipogusto varchar(100) not null
+);
 
-create table gustosMusica(
-idGustoMusica serial primary key,
-nombre varchar(100) not null,
-descripcion varchar(500),
-usuario varchar(100) references usuarios(correo));
+alter table tipo_gusto add constraint pk_tgusto primary key (id_tipoGusto);
+
+
+create table gustos(
+descripciongusto varchar(100) not null,
+usuario varchar(100) not null,
+tipogusto numeric(10) not null
+);
+
+alter table gustos add constraint fk_gustos1 foreign key (usuario) references usuarios(correo);
+alter table gustos add constraint fk_gustos2 foreign key (tipogusto) references tipo_gusto(id_tipoGusto);
 
 create table intereses(
-sexoInteres char(1) not null,
+sexoInteres char(100) not null,
 tipoRelacion varchar(100) not null,
 aspectosImportantes varchar(600),
-usuario varchar(100) references usuarios(correo));
+usuario varchar(100) not null);
+
+alter table intereses add constraint fk_intereses foreign key (usuario) references usuarios(correo);
+
+
+create table interacciones(
+usuario varchar(100) not null,
+pareja varchar(100) not null,
+likes numeric(1) not null
+);
+
+
+alter table interacciones add constraint fk_interacciones1 foreign key (usuario) references usuarios(correo);
+alter table interacciones add constraint fk_interacciones2 foreign key (pareja) references usuarios(correo);
 
 create table matches(
-correo varchar(100) primary key,
-nombre varchar(50) not null,
-fechaMatch date not null);
+usuario varchar(100) not null,
+pareja varchar(100) not null,
+fecha date not null
+);
+
+alter table matches add constraint fk_matches1 foreign key (usuario) references usuarios(correo);
+alter table matches add constraint fk_matches2 foreign key (pareja) references usuarios(correo);
+
