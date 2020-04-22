9d939227371927bf9aeaa5fbead8d7c661465435
jnicolasct <johan.cortes@mail.escuelaing.edu.co>
2020-04-21 22:05:40 -0500

    front de visualizacion y actualizacion del perfil del usuario

--- src/main/resources/static/js/apiUser.js
+++ src/main/resources/static/js/apiUser.js
@@ -67,10 +67,33 @@ apiUser = (function(){
 	        location.href = "/perfil.html";
 	    },
 	    
-	    irMatch : function () {
+	    irACBasica : function () {
+	        location.href = "/ACBasica.html";
+	    },
+            irACCarrera : function () {
+	        location.href = "/ACCarrera.html";
+	    },
+            irACFetiches : function () {
+	        location.href = "/ACFetiches.html";
+	    },
+            irACGComida : function () {
+	        location.href = "/ACGustosComida.html";
+	    },
+	    irACGMusica : function () {
+	        location.href = "/ACGustosMusicales.html";
+	    },
+            irACGPeliculas : function () {
+	        location.href = "/ACGustosPeliculas.html";
+	    },
+            irACIntereses : function () {
+	        location.href = "/ACIntereses.html";
+	    },
+            irPerfilUsuario : function () {
+	        location.href = "/perfilUsuario.html";
+	    },
+            irMatch : function () {
 	        location.href = "/match.html";
 	    },
-	    
 	    irLogin : function () {
 	        location.href = "/index.html";
 	        sessionStorage.setItem('UserLogin',"");
