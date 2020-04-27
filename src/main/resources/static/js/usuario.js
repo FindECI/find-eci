bec2487769654cb90eac8504e4920c98f26d2b15
jnicolasct <johan.cortes@mail.escuelaing.edu.co>
2020-04-27 13:43:46 -0500

    restablecimiento de front configuracion y mostrar los datos

--- src/main/resources/static/js/usuario.js
+++ src/main/resources/static/js/usuario.js
@@ -37,15 +37,80 @@ var usuario = (function () {
         var password = document.getElementById("password").value;
         apiUser.loginUser(user, password);
         sessionStorage.setItem('UserLogin',document.getElementById("username").value);
+        apiUser.getUser(infoUser, user);
+        apiCarrera.getCarrera(infoCarrera, user);
+        apiIntereses.getUserInteres(infoIntereses, user);
     };
-        
+    
+    var infoUser = function(datos){
+	 var nombre = datos.nombre;
+         var apellido = datos.apellido;
+         var genero = datos.sexo;
+         var altura = datos.altura;
+         var celular = datos.celular;
+         var instagram = datos.cuentaig;
+         var fechaNacimiento = datos.fechaNacimiento;
+         sessionStorage.setItem('Nombre',nombre + " " +apellido);
+         sessionStorage.setItem('Genero',genero);
+         sessionStorage.setItem('Altura',altura);
+         sessionStorage.setItem('Celular',celular);
+         sessionStorage.setItem('Instagram',instagram);
+         sessionStorage.setItem('Nacimiento',fechaNacimiento);
+    };
+    var infoCarrera = function(datos){
+	 var carrera = datos.nombre;
+         var semestre = datos.semestre;
+         sessionStorage.setItem('Carrera',carrera);
+         sessionStorage.setItem('Semestre',semestre);
+    };
+    var infoIntereses = function(datos){
+	 var generoInt = datos.sexoInteres;
+         var tipRel = datos.tipoRel;
+         sessionStorage.setItem('GeneroInt',generoInt);
+         sessionStorage.setItem('TipRel',tipRel);
+    };
+    
+    var cargueInfo = function(){
+	var correo = sessionStorage.getItem('UserLogin');
+        var nombre = sessionStorage.getItem('Nombre');
+        var genero = sessionStorage.getItem('Genero');
+        var altura = sessionStorage.getItem('Altura');
+        var celular = sessionStorage.getItem('Celular');
+        var instagram = sessionStorage.getItem('Instagram');
+        var fechaNacimiento = sessionStorage.getItem('Nacimiento');
+        var carrera = sessionStorage.getItem('Carrera');
+        var semestre = sessionStorage.getItem('Semestre');
+        var fechaActual = new Date();
+        var fechaN= new Date(fechaNacimiento);
+        var r1 = fechaN.getTime();
+        var r2 = fechaActual.getTime();
+        var fecha = r2-r1
+        var edad = Math.floor(fecha / 32000000000);
+        var generoInt = sessionStorage.getItem('GeneroInt');
+        var tipRel = sessionStorage.getItem('TipRel');
+        document.getElementById('email').innerHTML = correo;
+        document.getElementById('nombre').innerHTML = nombre;
+        document.getElementById('gene').innerHTML = genero;
+        document.getElementById('altura').innerHTML = altura;
+        document.getElementById('celu').innerHTML = celular;
+        document.getElementById('Igram').innerHTML = instagram;
+        document.getElementById('edad').innerHTML = edad;
+        document.getElementById('carrera').innerHTML = carrera;
+        document.getElementById('semestre').innerHTML = semestre;
+        document.getElementById('genInt').innerHTML = generoInt;
+        document.getElementById('tprel').innerHTML = tipRel;
+    };
     
     return {
 
         crearcuenta: crearcuentas,
         pageRegistrar: irRegistrar,
         ingresoApp: login,
-        crearCarrera : registraCarrera
+        crearCarrera : registraCarrera,
+        info: infoUser,
+        infoC: infoCarrera,
+        infoI: infoIntereses,
+        cargue: cargueInfo
 
     };
 })();
