d8ddbc8b779dd3a146bd5d112cfbd1682a1d324f
jnicolasct <47215172+jnicolasct@users.noreply.github.com>
2020-05-11 20:13:09 -0500

    correccion 1

--- src/main/java/edu/eci/arsw/findeci/persistence/InteresesRepository.java
+++ src/main/java/edu/eci/arsw/findeci/persistence/InteresesRepository.java
@@ -22,10 +22,10 @@ public interface InteresesRepository extends JpaRepository<Intereses,Integer>{
 	@Query(value="select i.id as id,i.aspectos_importantes as aspectos_importantes,i.sexo_interes as sexo_interes,i.tipo_relacion as tipo_relacion,i.usuario as usuario from intereses i where i.usuario <> :correo and i.sexo_interes = :sexo",nativeQuery = true)
 	List<Intereses> interesesOtherUser(String correo, String sexo);
         
-        @Transactional
+        /*@Transactional
         @Modifying
         @Query("update intereses i set i.aspectos_importantes= ?1, i.sexo_interes= ?2, i.tipo_relacion= ?3 where i.usuario= ?4")
-        void setIntereses(String aspectosImportantes, String sexoInt, String tipoRel, String correo);
+        void setIntereses(String aspectosImportantes, String sexoInt, String tipoRel, String correo);*/
 	
 	
 	
