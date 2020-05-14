2bcf08696ee982f6e63f1c33ace9935384c7cc2d
Andres <andres.quintero98@hotmail.com>
2020-05-13 19:38:50 -0500

    Imagenes ya suben a azure blob storage

--- src/main/java/edu/eci/arsw/findeci/services/impl/ImagenesServicesImpl.java
+++ src/main/java/edu/eci/arsw/findeci/services/impl/ImagenesServicesImpl.java
@@ -1,6 +1,10 @@
 package edu.eci.arsw.findeci.services.impl;
 
-
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.StorageException;
+import com.microsoft.azure.storage.blob.CloudBlobClient;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
 import edu.eci.arsw.findeci.Generar;
 import java.io.File;
 import java.io.FileInputStream;
@@ -18,11 +22,16 @@ import edu.eci.arsw.findeci.persistence.FindEciException;
 import edu.eci.arsw.findeci.persistence.ImagenesRepository;
 import edu.eci.arsw.findeci.services.ImagenesServices;
 import edu.eci.arsw.findeci.services.InteresesServices;
+import java.io.DataInputStream;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.InvalidKeyException;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -36,33 +45,61 @@ public class ImagenesServicesImpl implements ImagenesServices {
     public String saveImage(MultipartFile file) throws FindEciException {
         String ruta = "";
 
-        if (!file.isEmpty()){
+        String storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=imagenusuario;AccountKey=qjz2743gHPfAd2EBEfsdV8HkvLHXR7ZhYOnANMCnhOTnk7yI/6zW8YDvpdgF8S+/a3MobKf0Y/pwVmp7ya7b5A==;EndpointSuffix=core.windows.net";
+        CloudStorageAccount storageAccount = null;
+        CloudBlobContainer container = null;
+        Long le = file.getSize();
+        try {
+            storageAccount = CloudStorageAccount.parse(storageConnectionString);
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(ImagenesServicesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidKeyException ex) {
+            Logger.getLogger(ImagenesServicesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+        try {
+            container = blobClient.getContainerReference("prueba");
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(ImagenesServicesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (StorageException ex) {
+            Logger.getLogger(ImagenesServicesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        if (!file.isEmpty()) {
             byte[] bytes;
             try {
                 bytes = file.getBytes();
 
-                String nombre = Generar.code()+file.getOriginalFilename();
+                String nombre = Generar.code() + file.getOriginalFilename();
 
-                Path path = Paths.get(".//src//main//resources//static//img//"+nombre);
+                Path path = Paths.get(".//src//main//resources//static//img//" + nombre);
 
                 String tipo = file.getContentType();
+                InputStream is = null;
 
-                switch(tipo){
+                if(tipo != null) {
+                    is = new DataInputStream(file.getInputStream());
+                    long length = file.getSize();
+                    CloudBlockBlob blob = container.getBlockBlobReference("" + nombre);
+                    blob.upload(is, length);
+                }
+
+                switch (tipo) {
                     case "image/png":
                         Files.write(path, bytes);
-                        ruta = "/img/"+nombre;
+                        ruta = "/img/" + nombre;
                         break;
                     case "image/jpg":
                         Files.write(path, bytes);
-                        ruta = "/img/"+nombre;
+                        ruta = "/img/" + nombre;
                         break;
                     case "image/jpeg":
                         Files.write(path, bytes);
-                        ruta = "/img/"+nombre;
+                        ruta = "/img/" + nombre;
                         break;
                     case "image/gif":
                         Files.write(path, bytes);
-                        ruta = "/img/"+nombre;
+                        ruta = "/img/" + nombre;
                         break;
                     default:
                         ruta = "";
@@ -70,6 +107,10 @@ public class ImagenesServicesImpl implements ImagenesServices {
                 }
             } catch (IOException ex) {
                 ruta = "";
+            } catch (URISyntaxException ex) {
+                Logger.getLogger(ImagenesServicesImpl.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (StorageException ex) {
+                Logger.getLogger(ImagenesServicesImpl.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
         return ruta;
@@ -82,7 +123,7 @@ public class ImagenesServicesImpl implements ImagenesServices {
     }
 
     @Override
-    public List<Imagenes> find(Pageable page){
+    public List<Imagenes> find(Pageable page) {
         return imagenRepository.findAll(page).getContent();
     }
 
@@ -94,7 +135,5 @@ public class ImagenesServicesImpl implements ImagenesServices {
         img.setFecha(new Date());
         imagenRepository.save(img);
     }
-
-    
 
 }
