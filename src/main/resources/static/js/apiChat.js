09ebb45dd993abf8b1f6b32ccd809c04c57a269e
jimmy chirivi <jimmychirivi09@gmail.com>
2020-05-15 22:29:40 -0500

    update apichat

--- src/main/resources/static/js/apiChat.js
+++ src/main/resources/static/js/apiChat.js
@@ -23,6 +23,7 @@ var chat = (function() {
 	}
 
 	var onError = function(error) {
+		
 		connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
 		connectingElement.style.color = 'red';
 	}
